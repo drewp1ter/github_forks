cde6d236b902aa9927fbce402a28c6d53deca127
mufdvr <belora2010@yandex.ru>
2019-04-17 03:43:39 +0300

    add proxy

--- src/features/gitForks/apiEndpoints.ts
+++ src/features/gitForks/apiEndpoints.ts
@@ -1,7 +1,5 @@
 import { IForksRequest } from './models'
 
-const { REACT_APP_API_PLACEHOLDER } = process.env
-
-export const userRepos = (name: string): string => `${REACT_APP_API_PLACEHOLDER}users/${name}/repos`
+export const userRepos = (name: string): string => `/users/${name}/repos`
 export const repoForks = ({ userName, repoName, page = 0 }: IForksRequest) =>
-  `${REACT_APP_API_PLACEHOLDER}repos/${userName}/${repoName}/forks?page=${page}`
+  `/repos/${userName}/${repoName}/forks?page=${page}`
